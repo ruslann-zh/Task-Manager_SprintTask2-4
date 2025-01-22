fb8b14d4796f234f55163116162f3c19c25e922d
ruslann-zh <84094995+ruslann-zh@users.noreply.github.com>
2025-01-23 03:02:13 +0500

    Add files via upload

---
+++ SprintTask2_4/DeleteServlet.java
@@ -0,0 +1,21 @@
+package SprintTask2_4;
+
+import SprintTask2_4.db.DBManager;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(value = "/delete")
+public class DeleteServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Long id = Long.valueOf(req.getParameter("idshka"));
+        DBManager.deleteTask(id);
+
+        resp.sendRedirect("/task1");
+    }
+}
